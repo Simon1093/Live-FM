f135043d297a8ed6a385567e4546824b91551fca
Tokarev Roman <79996969490@mail.ru>
2019-03-05 23:49:49 +0300

    Fix Https Middleware

--- public_html/app/Http/Middleware/HttpsProtocol.php
+++ public_html/app/Http/Middleware/HttpsProtocol.php
@@ -10,7 +10,7 @@ class HttpsProtocol {
     public function handle($request, Closure $next)
     {
         $request->setTrustedProxies([$request->getClientIp()], Request::HEADER_X_FORWARDED_ALL);
-        if (!$request->secure() && env('APP_ENV') === 'prod') {
+        if (!$request->secure() && env('APP_ENV') != 'local') {
             return redirect()->secure($request->getRequestUri());
         }
         return $next($request); 
